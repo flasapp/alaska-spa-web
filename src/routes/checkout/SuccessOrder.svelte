cd737f32f44684ebe046837b51193148b3f3cded
flasapp <flasap@gmail.com>
2025-05-06 12:59:07 -0300

    update

--- src/routes/checkout/SuccessOrder.svelte
+++ src/routes/checkout/SuccessOrder.svelte
@@ -20,8 +20,12 @@
 
 <div class="pointer-events-auto w-full bg-base-100">
 
-	<div class="px-4 py-6 sm:px-6">
-		<h3 class="font-light w-full text-center">Gracias por tu compra <span class="font-regular">{orderBody.user.name}</span>, su pedido se ha guardado con éxito.</h3>
+	<div class="px-4 py-6 sm:px-6 font-light w-full text-left">
+		Gracias por tu compra <span class="font-bold">{orderBody.user.name}</span>, 
+		<br><span class="text-sm font-medium">Hemos enviado un correo a su casilla, no olvides revisar tu bandeja de spam.</span>
+		
+
+
 		<SuccessAnimation />
 		<p class="font-regular pt-2">
 			Dirección de entrega: 
@@ -30,19 +34,19 @@
 				{#if orderBody.user.depto} / {orderBody.user.depto}{/if}
 			</span>
 		</p>
-		<p class="font-regular pt-2">
+		<p class="font-regular pt-1">
 			Fecha de entrega: 
 			<span class="font-light">
 				{formatDate(orderBody.schedule.day)}
 			</span>
 		</p>
-		<p class="font-regular pt-2">
+		<p class="font-regular pt-1">
 			Horario de entrega: 
 			<span class="font-light">
 				{orderBody.schedule.hour}
 			</span>
 		</p>
-		<p class="font-medium pt-4 font-md">Muchas gracias!</p>
+		<p class="font-medium pt-2 font-md">Muchas gracias!</p>
 		<p style="font-size: .4rem; visibility:hidden;">Que disfrute sus productos, y si no le gustan vaya a lavarse las patas!!!</p>
 	</div>
 </div>
