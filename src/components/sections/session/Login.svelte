fbd07e408ce984513045c01348a7ef302cfbd426
flasapp <flasap@gmail.com>
2026-01-21 12:47:52 -0300

    update

--- src/components/sections/session/Login.svelte
+++ src/components/sections/session/Login.svelte
@@ -1,48 +1,50 @@
 <script>
 	//Core
-	import { fade, fly } from 'svelte/transition';
+	import { fade, fly } from "svelte/transition";
 	//Components
-	import Input from "@/components/form/Input.svelte"
-	import PasswordIcon from "@/components/shared/icons/Password.svelte"
-	import MailIcon from "@/components/shared/icons/Mail.svelte"
+	import Input from "@/components/form/Input.svelte";
+	import PasswordIcon from "@/components/shared/icons/Password.svelte";
+	import MailIcon from "@/components/shared/icons/Mail.svelte";
 	//Utils and Stores
-	import { post } from "@/lib/methods/api"
-	import GeneralStore from "@/stores/General"
-	import { addToast } from "@/stores/Toasts"
+	import { post } from "@/lib/methods/api";
+	import GeneralStore from "@/stores/General";
+	import { addToast } from "@/stores/Toasts";
 
 	//Local Storage
 	// const userLogged = browser && localStorage?.getItem('userLogged') ? JSON.parse(localStorage?.getItem('cart')) : []
 
 	//Variables
-	let submitted = false
-	let invalidMail = ''
-	let invalidPass = ''
+	let submitted = false;
+	let invalidMail = "";
+	let invalidPass = "";
 
 	let user = {
 		mail: "",
-		pass: ""
-	}
+		pass: "",
+	};
 
 	//Computed
-	$: invalidMail = user.mail == '' && submitted
-	$: invalidPass = user.pass == '' && submitted
+	$: invalidMail = user.mail == "" && submitted;
+	$: invalidPass = user.pass == "" && submitted;
 
 	//Functions
-	function setLoggedUser(user){
-		if(user){
-			$GeneralStore.userLogged = user
-			localStorage.setItem('userLogged', JSON.stringify(user))
-		} 
+	function setLoggedUser(user) {
+		if (user) {
+			$GeneralStore.userLogged = user;
+			localStorage.setItem("userLogged", JSON.stringify(user));
+		}
 	}
 
 	async function login() {
-		submitted = true
-		if(!user.mail || !user.pass) return 
-		// let resp = {}
-		let response = await post('login', user)
-		console.log("🚀  --> response:", response)
-		submitted = false
-		if (!response.id) return addToast({ text: "Email y/o contraseña incorrecta", type: "Error" })
+		submitted = true;
+		if (!user.mail || !user.pass) return;
+		let response = await post("login", user);
+		submitted = false;
+		if (!response.id)
+			return addToast({
+				text: "Email y/o contraseña incorrecta",
+				type: "Error",
+			});
 		const userLogged = {
 			id: response.id,
 			name: response.name,
@@ -55,30 +57,43 @@
 				number: response.number,
 				depto: response.apartment,
 				corner: response.corner,
-				neighbourhood: response.neighborhoodId
-			}
-		}
-		setLoggedUser(userLogged)
+				neighbourhood: response.neighborhoodId,
+			},
+		};
+		setLoggedUser(userLogged);
 		addToast({ text: "Logueado con éxito", type: "success" });
 		return response;
-  	}
-
-
+	}
 </script>
 
 <div in:fade={{ delay: 200 }}>
-	<div 
-		class="rounded flex-grow place-items-center">
+	<div class="rounded flex-grow place-items-center">
 		<!-- <div class="w-full text-left mb-4">
 			<h2>Ingresa como usuario registrado!</h2>
 		</div> -->
 		<form action="" class="w-full">
 			<div class="form-control">
-				<Input placeholder="Email" bind:value={user.mail} type="email" icon="{MailIcon}" invalidInput="{invalidMail}"/>
-				<br>
-				<Input placeholder="Contraseña" bind:value={user.pass} type="password" icon="{PasswordIcon}" invalidInput="{invalidPass}"/>
-				<br>
-				<button class="flex rounded-md font-medium btn btn-primary w-full" on:click={login} disabled={submitted}>
+				<Input
+					placeholder="Email"
+					bind:value={user.mail}
+					type="email"
+					icon={MailIcon}
+					invalidInput={invalidMail}
+				/>
+				<br />
+				<Input
+					placeholder="Contraseña"
+					bind:value={user.pass}
+					type="password"
+					icon={PasswordIcon}
+					invalidInput={invalidPass}
+				/>
+				<br />
+				<button
+					class="flex rounded-md font-medium btn btn-primary w-full"
+					on:click={login}
+					disabled={submitted}
+				>
 					{#if submitted}
 						<span class="loading loading-spinner"></span>
 						Ingresando
@@ -86,12 +101,16 @@
 						Ingresar
 					{/if}
 				</button>
-				<div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-				
-					<a type="button" class="font-regular text-sm text-primary" href="/recuperar-usuario/obtener-codigo">
+				<div
+					class="mt-6 flex justify-center text-center text-sm text-gray-500"
+				>
+					<a
+						type="button"
+						class="font-regular text-sm text-primary"
+						href="/recuperar-usuario/obtener-codigo"
+					>
 						Olvidaste la contraseña?
 					</a>
-					
 				</div>
 			</div>
 		</form>
--- src/routes/profile/Profile.svelte
+++ src/routes/profile/Profile.svelte
@@ -1,33 +1,33 @@
 <script>
 	//Core
-	import { onMount } from "svelte"
+	import { onMount } from "svelte";
 	//Utils and stores
-	import { router } from 'tinro';
-	import { post, get } from "@/lib/methods/api"
-	import GeneralSettings, { updateUser } from "@/stores/General"
-	import { addToast } from "@/stores/Toasts"
+	import { router } from "tinro";
+	import { post, get } from "@/lib/methods/api";
+	import GeneralSettings, { updateUser } from "@/stores/General";
+	import { addToast } from "@/stores/Toasts";
 
 	//Components
-	import Input from "@/components/form/Input.svelte"
-	import PhoneIcon from "@/components/shared/icons/Phone.svelte"
-	import MailIcon from "@/components/shared/icons/Mail.svelte"
-	import UserIcon from "@/components/shared/icons/User.svelte"
-	import AddressIcon from "@/components/shared/icons/AddressBook.svelte"
-	import BuilindgIcon from "@/components/shared/icons/Building.svelte"
-	import CornerIcon from "@/components/shared/icons/Corner.svelte"
-	import LocationIcon from "@/components/shared/icons/Location.svelte"
-	import RighArrowIcon from "@/components/shared/icons/RightArrow.svelte"
-	import InfoIcon from "@/components/shared/icons/Info.svelte"
+	import Input from "@/components/form/Input.svelte";
+	import PhoneIcon from "@/components/shared/icons/Phone.svelte";
+	import MailIcon from "@/components/shared/icons/Mail.svelte";
+	import UserIcon from "@/components/shared/icons/User.svelte";
+	import AddressIcon from "@/components/shared/icons/AddressBook.svelte";
+	import BuilindgIcon from "@/components/shared/icons/Building.svelte";
+	import CornerIcon from "@/components/shared/icons/Corner.svelte";
+	import LocationIcon from "@/components/shared/icons/Location.svelte";
+	import RighArrowIcon from "@/components/shared/icons/RightArrow.svelte";
+	import InfoIcon from "@/components/shared/icons/Info.svelte";
 
 	//Variables for tabs
-	let active_tab = 'INFO'
+	let active_tab = "INFO";
 	//Variables for invalid inputs
-	let invalidForm = {}
-	let saving = false
-	let orders = []
+	let invalidForm = {};
+	let saving = false;
+	let orders = [];
 	//Functions
 	const restoreInvalids = () => {
-		saving = false
+		saving = false;
 		invalidForm = {
 			name: false,
 			lastName: false,
@@ -37,183 +37,260 @@
 			number: false,
 			corner: false,
 			depto: false,
-			neighbourhood: false
-		}
-	}
+			neighbourhood: false,
+		};
+	};
 
-	const save = async () =>{
-		restoreInvalids()
+	const save = async () => {
+		restoreInvalids();
 		//Validate name, lastname, email, phone, street, number, neighborhood
-		if(!$GeneralSettings.userLogged.name) return invalidForm.name = true
-		if(!$GeneralSettings.userLogged.lastName) return invalidForm.lastName = true
-		if(!$GeneralSettings.userLogged.email) return invalidForm.email = true
-		if(!$GeneralSettings.userLogged.phone) return invalidForm.phone = true
-		if(!$GeneralSettings.userLogged.address.street) return invalidForm.street = true
-		if(!$GeneralSettings.userLogged.address.number) return invalidForm.number = true
-		if(!$GeneralSettings.userLogged.address.neighbourhood) return invalidForm.neighborhood = true
-		
+		if (!$GeneralSettings.userLogged.name) return (invalidForm.name = true);
+		if (!$GeneralSettings.userLogged.lastName)
+			return (invalidForm.lastName = true);
+		if (!$GeneralSettings.userLogged.email)
+			return (invalidForm.email = true);
+		if (!$GeneralSettings.userLogged.phone)
+			return (invalidForm.phone = true);
+		if (!$GeneralSettings.userLogged.address.street)
+			return (invalidForm.street = true);
+		if (!$GeneralSettings.userLogged.address.number)
+			return (invalidForm.number = true);
+		if (!$GeneralSettings.userLogged.address.neighbourhood)
+			return (invalidForm.neighborhood = true);
+
 		//Save user data
-		updateUser($GeneralSettings.userLogged)
-		saving = true
-		let response = await post(`update-profile`, $GeneralSettings.userLogged)
-		saving = false
-		console.log("🚀  --> response:", response)
-		if(response.success) return addToast({ text: "Datos actualizados correctamente", type: "Success" })
-		addToast({ text: "Ha ocurrido un error, intentelo nuevamente mas tarde", type: "Error" })
-	
-	}
+		updateUser($GeneralSettings.userLogged);
+		saving = true;
+		let response = await post(
+			`update-profile`,
+			$GeneralSettings.userLogged,
+		);
+		saving = false;
+		if (response.success)
+			return addToast({
+				text: "Datos actualizados correctamente",
+				type: "Success",
+			});
+		addToast({
+			text: "Ha ocurrido un error, intentelo nuevamente mas tarde",
+			type: "Error",
+		});
+	};
 
-	const formatDate = (date) =>{
-		const newDate = date
-		const splittedDate = newDate.split('-')
-		let formattedDate = !splittedDate[2] || !splittedDate[1] || !splittedDate[0] ? "--/--/----" : `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+	const formatDate = (date) => {
+		const newDate = date;
+		const splittedDate = newDate.split("-");
+		let formattedDate =
+			!splittedDate[2] || !splittedDate[1] || !splittedDate[0]
+				? "--/--/----"
+				: `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
 		// Formateamos la fecha como DD/MM/YYYY
-		return formattedDate
-
-	}
+		return formattedDate;
+	};
 
 	const getMyOrders = async () => {
-		setTimeout(async () => { // --> This is a workaround if the user reload the page. The userLogged is not available yet
-			if(!$GeneralSettings.userLogged.id) return
-			let resp = await get(`orders-user/${$GeneralSettings.userLogged.id}`)
-			orders = resp.data.filter(order => order.total != '0') // --> This avoid to show orders with total 0 (old data)
-			orders.forEach( (order) => {
-				order.deliveryDateToDisplay = formatDate(order.deliveryDate)
-			})
+		setTimeout(async () => {
+			// --> This is a workaround if the user reload the page. The userLogged is not available yet
+			if (!$GeneralSettings.userLogged.id) return;
+			let resp = await get(
+				`orders-user/${$GeneralSettings.userLogged.id}`,
+			);
+			orders = resp.data.filter((order) => order.total != "0"); // --> This avoid to show orders with total 0 (old data)
+			orders.forEach((order) => {
+				order.deliveryDateToDisplay = formatDate(order.deliveryDate);
+			});
 		}, 200);
-	}
-	
+	};
+
 	const getNeighbourhoods = async () => {
-		let resp = await get('settings/neighbourhoods');
-		let neighbourhoods = resp?.data?.map( (neigh)=>{
+		let resp = await get("settings/neighbourhoods");
+		let neighbourhoods = resp?.data?.map((neigh) => {
 			return {
 				id: neigh.id,
 				title: neigh.title,
-				amount: neigh.amount
-			}	
-		})
-		$GeneralSettings.neighbourhoods = neighbourhoods
-	}
-	
+				amount: neigh.amount,
+			};
+		});
+		$GeneralSettings.neighbourhoods = neighbourhoods;
+	};
+
 	const goTo = (orderId) => {
-		router.goto(`/pedidos/${orderId}/${$GeneralSettings.userLogged.id}`)
-	}
+		router.goto(`/pedidos/${orderId}/${$GeneralSettings.userLogged.id}`);
+	};
 
 	onMount(async () => {
-		
-		if($GeneralSettings.neighbourhoods.length == 0) getNeighbourhoods()
-		getMyOrders()
+		if ($GeneralSettings.neighbourhoods.length == 0) getNeighbourhoods();
+		getMyOrders();
 		// if(!$GeneralSettings.userLogged.id){
 		// 	addToast({ text: "Debes iniciar sesión para acceder a tu perfil", type: "Error" })
-		// 	goto(`/`) 
-		// } 
-	})
+		// 	goto(`/`)
+		// }
+	});
 </script>
 
-<div class="mx-auto mt-4 py-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 relative text-center">
-    <div class="text-sm breadcrumbs py-4">
-      	<ul>
-        	<li><a href="/">Inicio</a></li>
-        	<li>Perfil</li>
-        	{#if active_tab == 'INFO'}<li>Mis datos</li>{/if}
-			{#if active_tab == 'ORDERS'}<li>Mis Pedidos</li>{/if}
-      	</ul>
-    </div>
+<div
+	class="mx-auto mt-4 py-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 relative text-center"
+>
+	<div class="text-sm breadcrumbs py-4">
+		<ul>
+			<li><a href="/">Inicio</a></li>
+			<li>Perfil</li>
+			{#if active_tab == "INFO"}<li>Mis datos</li>{/if}
+			{#if active_tab == "ORDERS"}<li>Mis Pedidos</li>{/if}
+		</ul>
+	</div>
 
 	<div class="text-left">
-    	<h2 class="text-2xl font-medium">Perfil</h2>
+		<h2 class="text-2xl font-medium">Perfil</h2>
 		<div>
 			<div class="w-full lg:w-1/2 m-auto text-left mt-4">
-				<div class="bg-gradient-to-r from-violet-400 to-blue-400 p-1 rounded">
+				<div
+					class="bg-gradient-to-r from-violet-400 to-blue-400 p-1 rounded"
+				>
 					<div class="bg-base-100 p-4">
 						<div role="tablist" class="tabs tabs-boxed bg-base-100">
-							<a role="tab" class="tab {active_tab == 'INFO' ? 'tab-active' : ''}" on:click={() => {active_tab = 'INFO'}}>Mis datos</a>
-							<a role="tab" class="tab {active_tab == 'ORDERS' ? 'tab-active' : ''}" on:click={() => {active_tab = 'ORDERS'; getMyOrders();}}>Mis pedidos</a>
+							<a
+								role="tab"
+								class="tab {active_tab == 'INFO'
+									? 'tab-active'
+									: ''}"
+								on:click={() => {
+									active_tab = "INFO";
+								}}>Mis datos</a
+							>
+							<a
+								role="tab"
+								class="tab {active_tab == 'ORDERS'
+									? 'tab-active'
+									: ''}"
+								on:click={() => {
+									active_tab = "ORDERS";
+									getMyOrders();
+								}}>Mis pedidos</a
+							>
 						</div>
-						<div class="pointer-events-auto w-full m-auto" >
-							{#if active_tab == 'INFO'}
+						<div class="pointer-events-auto w-full m-auto">
+							{#if active_tab == "INFO"}
 								<!-- <Login /> -->
-								 <form action="" class="w-full">
-									<div class="form-control grid gap-2 text-left p-4">
+								<form action="" class="w-full">
+									<div
+										class="form-control grid gap-2 text-left p-4"
+									>
 										<!-- Hola {$GeneralSettings.userLogged.name}! tu dir es {delivery.street} {delivery.number} {delivery.corner} {delivery.depto} -->
-										<Input 
-											placeholder="Nombre" 
-											bind:value={$GeneralSettings.userLogged.name} 
-											icon="{UserIcon}" 
-											invalidInput={invalidForm.name} 
-											label="Nombre" 
+										<Input
+											placeholder="Nombre"
+											bind:value={
+												$GeneralSettings.userLogged.name
+											}
+											icon={UserIcon}
+											invalidInput={invalidForm.name}
+											label="Nombre"
 										/>
-										<Input 
-											placeholder="Apellido" 
-											bind:value={$GeneralSettings.userLogged.lastName} 
-											icon="{UserIcon}" 
-											invalidInput={invalidForm.lastName} 
-											label="Apellido" 
+										<Input
+											placeholder="Apellido"
+											bind:value={
+												$GeneralSettings.userLogged
+													.lastName
+											}
+											icon={UserIcon}
+											invalidInput={invalidForm.lastName}
+											label="Apellido"
 										/>
 
 										<Input
 											placeholder="Email"
-											bind:value={$GeneralSettings.userLogged.email}
-											icon="{MailIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.email
+											}
+											icon={MailIcon}
 											invalidInput={invalidForm.email}
 											label="Email"
 										/>
 										<Input
 											placeholder="Teléfono"
-											bind:value={$GeneralSettings.userLogged.phone}
-											icon="{PhoneIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.phone
+											}
+											icon={PhoneIcon}
 											invalidInput={invalidForm.phone}
 											label="Teléfono"
 										/>
 										<Input
 											placeholder="Calle"
-											bind:value={$GeneralSettings.userLogged.address.street}
-											icon="{LocationIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.address.street
+											}
+											icon={LocationIcon}
 											invalidInput={invalidForm.street}
 											label="Calle"
 										/>
 										<Input
 											placeholder="Número"
-											bind:value={$GeneralSettings.userLogged.address.number}
-											icon="{BuilindgIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.address.number
+											}
+											icon={BuilindgIcon}
 											invalidInput={invalidForm.number}
 											label="Número"
 										/>
 										<Input
 											placeholder="Esquina"
-											bind:value={$GeneralSettings.userLogged.address.corner}
-											icon="{CornerIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.address.corner
+											}
+											icon={CornerIcon}
 											invalidInput={invalidForm.corner}
 											label="Esquina"
 										/>
 										<Input
 											placeholder="Depto"
-											bind:value={$GeneralSettings.userLogged.address.depto}
-											icon="{AddressIcon}"
+											bind:value={
+												$GeneralSettings.userLogged
+													.address.depto
+											}
+											icon={AddressIcon}
 											invalidInput={invalidForm.depto}
 											label="Depto"
 										/>
 										<label>
 											<div class="label">
-												<span class="label-text">Barrio</span>
+												<span class="label-text"
+													>Barrio</span
+												>
 											</div>
-											<select 
-												class="select select-bordered w-full" 
-												bind:value={$GeneralSettings.userLogged.address.neighbourhood}
+											<select
+												class="select select-bordered w-full"
+												bind:value={
+													$GeneralSettings.userLogged
+														.address.neighbourhood
+												}
 												placeholder="Seleccione un barrio"
 											>
-												<option disabled selected>Seleccione su barrio</option>
+												<option disabled selected
+													>Seleccione su barrio</option
+												>
 												{#each $GeneralSettings.neighbourhoods as neighbourhood}
-													<option value={neighbourhood.id}>{neighbourhood.title}</option>
+													<option
+														value={neighbourhood.id}
+														>{neighbourhood.title}</option
+													>
 												{/each}
 											</select>
 										</label>
-										<button 
-											on:click={save} 
+										<button
+											on:click={save}
 											class="rounded-md font-medium btn btn-primary w-full mt-4"
-											disabled={saving}>
+											disabled={saving}
+										>
 											{#if saving}
-												<span class="loading loading-spinner"></span>
+												<span
+													class="loading loading-spinner"
+												></span>
 												Actualizando
 											{:else}
 												Actualizar mis datos
@@ -222,12 +299,17 @@
 									</div>
 								</form>
 							{/if}
-							{#if active_tab == 'ORDERS'}
+							{#if active_tab == "ORDERS"}
 								{#key orders}
 									{#if orders.length == 0}
-										<div role="alert" class="alert alert-info mt-4">
+										<div
+											role="alert"
+											class="alert alert-info mt-4"
+										>
 											<InfoIcon />
-											<span>No se encontraron pedidos.</span>
+											<span
+												>No se encontraron pedidos.</span
+											>
 										</div>
 										<p class="text-center"></p>
 									{:else}
@@ -237,7 +319,8 @@
 													<tr class="text-center">
 														<!-- <th class="">Pedido #</th> -->
 														<th class="">Fecha</th>
-														<th class="">Horario</th>
+														<th class="">Horario</th
+														>
 														<th class="">Total</th>
 														<th class="">Estado</th>
 														<th></th>
@@ -245,22 +328,38 @@
 												</thead>
 												<tbody>
 													{#each orders as order}
-														<tr class="text-center hover" on:click={goTo(order.orderId)}>
+														<tr
+															class="text-center hover"
+															on:click={goTo(
+																order.orderId,
+															)}
+														>
 															<!-- <td class="">{order.orderId}</td> -->
-															<td class="">{order.deliveryDateToDisplay}</td>
-															<td class="">{order.deliverySchedule}</td>
-															<td class="">$ {order.total}</td>
+															<td class=""
+																>{order.deliveryDateToDisplay}</td
+															>
+															<td class=""
+																>{order.deliverySchedule}</td
+															>
+															<td class=""
+																>$ {order.total}</td
+															>
 															<td class="">
 																{#if order.status == 1}
-																	<span class="badge badge-outline badge-primary">Procesado</span>
+																	<span
+																		class="badge badge-outline badge-primary"
+																		>Procesado</span
+																	>
 																{:else if order.status == 0}
-																	<span class="badge badge-outline badge-secondary">Pendiente</span>
+																	<span
+																		class="badge badge-outline badge-secondary"
+																		>Pendiente</span
+																	>
 																{/if}
 															</td>
 															<td class="">
-																
-																<RighArrowIcon />
-																
+																<RighArrowIcon
+																/>
 															</td>
 														</tr>
 													{/each}
@@ -274,13 +373,12 @@
 					</div>
 				</div>
 			</div>
-			
 		</div>
 	</div>
 </div>
 
 <style>
-	html{
+	html {
 		scroll-behavior: smooth;
 	}
 </style>
